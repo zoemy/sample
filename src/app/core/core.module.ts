3f3f7e1d46843fd0a10975580663f561c9024f23
zoemy <claudia.montalvo@outlook.com>
2018-03-15 06:51:17 -0500

    change

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 
+import { BaseService } from './base.service';
 import { MessageBoxService } from './message-box/message-box.service';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth.guard';
@@ -11,6 +12,6 @@ import { AngularFirestoreModule } from 'angularfire2/firestore';
     AngularFireAuthModule,
     AngularFirestoreModule 
   ],
-  providers: [AuthService,AuthGuard,MessageBoxService] 
+  providers: [AuthService,AuthGuard,MessageBoxService,BaseService] 
 })
 export class CoreModule { }
